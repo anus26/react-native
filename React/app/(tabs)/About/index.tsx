9cee226e34ba282df64622f927d6e0fd9cdee098
anus26 <anusraza268gmail.com>
2024-10-16 20:42:33 -0700

    create hello world

---
+++ React/app/(tabs)/About/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text, } from 'react-native'
+import React from 'react'
+import {} from "module";
+const About = () => {
+  return (
+    <View>
+      <Text >About</Text>
+      <button><link rel="stylesheet" href="HomeScreen " /></button>
+    </View>
+  )
+}
+
+export default About
